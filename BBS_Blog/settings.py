4c078274dc4507fb8d3890c69fd67752f3521fac
lun <1961603747.@qq.com>
2020-04-03 17:27:15 +0800

    create models

--- BBS_Blog/settings.py
+++ BBS_Blog/settings.py
@@ -75,12 +75,26 @@ WSGI_APPLICATION = 'BBS_Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "blog",
+        'USER': 'root',
+        'PASSWORD': '1154607',
+        'HOST': "127.0.0.1",
+        'PORT': 3306
     }
 }
+
+AUTH_USER_MODEL = "app1.UserInfo"
+
+
 
 
 # Password validation
